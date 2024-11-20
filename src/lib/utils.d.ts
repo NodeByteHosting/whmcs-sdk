2fcf64864d3fd474ab1950bd6201d476afca5fd2
TheRealToxicDev <toxic.dev09@gmail.com>
2024-11-19 17:40:10 -0700

    fix(bruhh): broken module

--- src/lib/utils.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const utils: {
-    /**
-     * Extend properties of one object with one or more Objects
-     * @param obj Object to extend
-     * @returns Object
-     */
-    extend: <T>(obj: T, ...sources: Partial<T>[]) => T;
-};
